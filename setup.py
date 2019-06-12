5c09933b8e35e2c9d5017e2c8efc2584d342a13a
Jaxxtrend <jaxxtrend@gmail.com>
2019-06-12 15:28:00 +0500

    add reuirements in setup

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 import setuptools
+import os
+try:
+    # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:
+    # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+def load_requirements(fname):
+    reqs = parse_requirements(fname, session="test")
+    return [str(ir.req) for ir in reqs]
+
+setuptools.setup(name="NodeGraphQt", install_requires=load_requirements("requirements.txt"))
 
 from NodeGraphQt import __version__ as version
 
