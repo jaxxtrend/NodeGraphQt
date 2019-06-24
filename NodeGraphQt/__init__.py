bb43a299cab7a9cccade461615c9105a67535f9f
Jaxxtrend <jaxxtrend@gmail.com>
2019-06-25 01:32:55 +0500

    test

--- NodeGraphQt/__init__.py
+++ NodeGraphQt/__init__.py
@@ -46,13 +46,13 @@ __all__ = [
     'constants', 'setup_context_menu'
 ]
 
-try:
-    from .vendor.Qt import QtWidgets, QtGui, QtCore, QtCompat
-except ImportError as ie:
-    from .vendor.Qt import __version__ as qtpy_ver
-    from .vendor.Qt import QtWidgets, QtGui, QtCore, QtCompat
-    print('Cannot import "Qt.py" module falling back on '
-        '"NodeGraphQt.vendor.Qt ({})"'.format(qtpy_ver))
+# try:
+#     from Qt import QtWidgets, QtGui, QtCore, QtCompat
+# except ImportError as ie:
+#     from .vendor.Qt import __version__ as qtpy_ver
+#     from .vendor.Qt import QtWidgets, QtGui, QtCore, QtCompat
+#     print('Cannot import "Qt.py" module falling back on '
+#         '"NodeGraphQt.vendor.Qt ({})"'.format(qtpy_ver))
 
 from .base.graph import NodeGraph
 from .base.node import NodeObject, BaseNode, BackdropNode
